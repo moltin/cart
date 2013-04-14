fdaa67c18195f7e6792e95da16b4a8a41f81436d
Chris Harvey <chris@chrisnharvey.com>
2013-04-14 13:20:31 +0000

    Adding Cookie identifier

---
+++ src/Moltin/Cart/Identifier/Cookie.php
@@ -0,0 +1,26 @@
+<?php namespace Moltin\Cart\Identifier;
+
+class Cookie implements \Moltin\Cart\IdentifierInterface
+{
+    public function get()
+    {
+        if (isset($_COOKIE['cart_identifier'])) {
+
+            return $_COOKIE['cart_identifier'];
+
+        } else {
+
+            return $this->regenerate();
+
+        }
+    }
+
+    public function regenerate()
+    {
+        $identifier = md5(uniqid(null, true));
+
+        setcookie('cart_identifier', $identifier);
+
+        return $identifier;
+    }
+}
