920d4c346e123e6fd3e5a900003850d427a6112a
Chris Harvey <chris@chrisnharvey.com>
2013-04-14 13:21:13 +0000

    Adding barebones of Session storage class

---
+++ src/Moltin/Cart/Storage/Session.php
@@ -0,0 +1,31 @@
+<?php namespace Moltin\Cart\Storage;
+
+class Session implements \Moltin\Cart\StorageInterface
+{
+    protected $identifier;
+
+    public function __construct()
+    {
+        session_id() or session_start();
+    }
+
+    public function insertUpdate($id, $data)
+    {
+        
+    }
+    
+    public function remove($id)
+    {
+
+    }
+    
+    public function setIdentifier($identifier)
+    {
+        $this->identifier = $identifier;
+    }
+    
+    public function getIdentifier($identifier)
+    {
+        return $this->identifier;
+    }
+}
