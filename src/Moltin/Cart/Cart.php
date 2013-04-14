7a758587c1c547fe9fc1bfc95d24ea6150939e62
Chris Harvey <chris@chrisnharvey.com>
2013-04-14 09:28:54 +0000

    Initial commit of Cart class

---
+++ src/Moltin/Cart/Cart.php
@@ -0,0 +1,20 @@
+<?php namespace Moltin\Cart;
+
+class Cart
+{
+    protected $id;
+    protected $store;
+    protected $identifier;
+
+    public function __construct(StorageInterface $store, IdentifierInterface $identifier)
+    {
+        $this->store = $store;
+        $this->identifier = $identifier;
+
+        // Generate/retrieve identifier
+        $this->id = $this->identifier->getIdentifier();
+
+        // Let our storage class know which cart we're talking about
+        $this->store->setIdentifier($this->id);
+    }
+}
