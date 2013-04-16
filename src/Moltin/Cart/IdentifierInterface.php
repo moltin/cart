8c3d1f1dbc403e080dc5d9e32671742b3315d017
Chris Harvey <chris@chrisnharvey.com>
2013-04-16 19:58:42 +0000

    Adding doc blocks to interfaces

--- src/Moltin/Cart/IdentifierInterface.php
+++ src/Moltin/Cart/IdentifierInterface.php
@@ -2,9 +2,24 @@
 
 interface IdentifierInterface
 {
+    /**
+     * Get the current or new unique identifier
+     * 
+     * @return string The identifier
+     */
     public function get();
 
+    /**
+     * Regenerate the identifier
+     * 
+     * @return string The identifier
+     */
     public function regenerate();
 
+    /**
+     * Forget the identifier
+     * 
+     * @return void
+     */
     public function forget();
 }
--- src/Moltin/Cart/StorageInterface.php
+++ src/Moltin/Cart/StorageInterface.php
@@ -2,17 +2,55 @@
 
 interface StorageInterface
 {
+    /**
+     * Add or update an item in the cart
+     * 
+     * @param  Item   $item The item to insert or update
+     * @return void
+     */
     public function insertUpdate(Item $item);
 
+    /**
+     * Retrieve the cart data
+     * 
+     * @return array
+     */
     public function data();
 
+    /**
+     * Check if the item exists in the cart
+     * 
+     * @param  mixed  $id
+     * @return boolean
+     */
     public function has($id);
-    
+
+    /**
+     * Remove an item from the cart
+     * 
+     * @param  mixed $id
+     * @return void
+     */
     public function remove($id);
 
+    /**
+     * Destroy the cart
+     * 
+     * @return void
+     */
     public function destroy();
     
+    /**
+     * Set the cart identifier
+     * 
+     * @param string $identifier
+     */
     public function setIdentifier($identifier);
     
+    /**
+     * Return the current cart identifier
+     * 
+     * @return void
+     */
     public function getIdentifier();
 }
