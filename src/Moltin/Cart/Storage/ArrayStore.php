c8079ab409a411a5deafa51e29945f2115f295e7
Chris Harvey <chris@chrisnharvey.com>
2013-04-17 20:15:18 +0000

    Adding Array storage class

---
+++ src/Moltin/Cart/Storage/ArrayStore.php
@@ -0,0 +1,48 @@
+<?php namespace Moltin\Cart\Storage;
+
+use Moltin\Cart\Item;
+
+class ArrayStore implements \Moltin\Cart\StorageInterface
+{
+    protected $identifier;
+    protected static $cart = array();
+
+    public function insertUpdate(Item $item)
+    {
+        static::$cart[$this->id][$item->identifier] = $item;
+    }
+
+    public function data()
+    {
+        return static::$cart[$this->id];
+    }
+
+    public function has($id)
+    {
+        foreach ($this->data as $item) {
+
+            if ($item['id'] == $id) return true;
+
+        }
+    }
+    
+    public function remove($id)
+    {
+        unset(static::$cart[$this->id][$id]);
+    }
+
+    public function destroy()
+    {
+        unset(static::$cart[$this->id]);
+    }
+
+    public function setIdentifier($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+}
