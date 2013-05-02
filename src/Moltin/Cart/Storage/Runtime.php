03f90e08bab8c1dfe02bb944f75e4d4d733e7544
Chris Harvey <chris@chrisnharvey.com>
2013-05-02 16:54:35 +0100

    Adding Runtime Storage docs

--- src/Moltin/Cart/Storage/Runtime.php
+++ src/Moltin/Cart/Storage/Runtime.php
@@ -7,16 +7,33 @@ class Runtime implements \Moltin\Cart\StorageInterface
     protected $identifier;
     protected static $cart = array();
 
+    /**
+     * Add or update an item in the cart
+     * 
+     * @param  Item   $item The item to insert or update
+     * @return void
+     */
     public function insertUpdate(Item $item)
     {
         static::$cart[$this->id][$item->identifier] = $item;
     }
 
+    /**
+     * Retrieve the cart data
+     * 
+     * @return array
+     */
     public function &data()
     {
         return static::$cart[$this->id];
     }
 
+    /**
+     * Check if the item exists in the cart
+     * 
+     * @param  mixed  $id
+     * @return boolean
+     */
     public function has($identifier)
     {
         foreach (static::$cart[$this->id] as $item) {
@@ -26,6 +43,12 @@ class Runtime implements \Moltin\Cart\StorageInterface
         }
     }
 
+    /**
+     * Get a single cart item by id
+     * 
+     * @param  mixed $id The item id
+     * @return Item  The item class
+     */
     public function item($identifier)
     {
         foreach (static::$cart[$this->id] as $item) {
@@ -35,16 +58,32 @@ class Runtime implements \Moltin\Cart\StorageInterface
         }
     }
     
+    /**
+     * Remove an item from the cart
+     * 
+     * @param  mixed $id
+     * @return void
+     */
     public function remove($id)
     {
         unset(static::$cart[$this->id][$id]);
     }
 
+    /**
+     * Destroy the cart
+     * 
+     * @return void
+     */
     public function destroy()
     {
         static::$cart[$this->id] = array();
     }
 
+    /**
+     * Set the cart identifier
+     * 
+     * @param string $identifier
+     */
     public function setIdentifier($id)
     {
         $this->id = $id;
@@ -54,6 +93,11 @@ class Runtime implements \Moltin\Cart\StorageInterface
         }
     }
 
+    /**
+     * Return the current cart identifier
+     * 
+     * @return void
+     */
     public function getIdentifier()
     {
         return $this->identifier;
