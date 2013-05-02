0d5e320b3a295a198213903265025099d13a955f
Chris Harvey <chris@chrisnharvey.com>
2013-05-02 17:01:13 +0100

    Adding session store docs

--- src/Moltin/Cart/Storage/Session.php
+++ src/Moltin/Cart/Storage/Session.php
@@ -4,6 +4,9 @@ use Moltin\Cart\Item;
 
 class Session extends Runtime implements \Moltin\Cart\StorageInterface
 {
+    /**
+     * The Session store constructor
+     */
     public function __construct()
     {
         session_id() or session_start();
@@ -11,6 +14,9 @@ class Session extends Runtime implements \Moltin\Cart\StorageInterface
         if (isset($_SESSION['cart'])) static::$cart = unserialize($_SESSION['cart']);
     }
 
+    /**
+     * The session store destructor.
+     */
     public function __destruct()
     {
         $_SESSION['cart'] = serialize(static::$cart);
