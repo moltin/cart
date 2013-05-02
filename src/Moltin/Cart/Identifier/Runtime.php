94b4e7cb8c799441d8105161dbfb84ad4cfdea27
Chris Harvey <chris@chrisnharvey.com>
2013-05-02 16:53:18 +0100

    Adding Runtime Identifier docs

--- src/Moltin/Cart/Identifier/Runtime.php
+++ src/Moltin/Cart/Identifier/Runtime.php
@@ -4,6 +4,11 @@ class Runtime implements \Moltin\Cart\IdentifierInterface
 {
     protected static $identifier;
 
+    /**
+     * Get the current or new unique identifier
+     * 
+     * @return string The identifier
+     */
     public function get()
     {
         if (isset(static::$identifier)) return static::$identifier;
@@ -11,6 +16,11 @@ class Runtime implements \Moltin\Cart\IdentifierInterface
         return $this->regenerate();
     }
 
+    /**
+     * Regenerate the identifier
+     * 
+     * @return string The identifier
+     */
     public function regenerate()
     {
         $identifier = md5(uniqid(null, true));
@@ -20,6 +30,11 @@ class Runtime implements \Moltin\Cart\IdentifierInterface
         return $identifier;
     }
 
+    /**
+     * Forget the identifier
+     * 
+     * @return void
+     */
     public function forget()
     {
         unset(static::$identifier);
