4fe2e79f9b8beaac4c575bdd9d59406d54fa787c
Chris Harvey <chris@chrisnharvey.com>
2013-05-02 16:55:50 +0100

    Session Storage now extends runtime storage

--- src/Moltin/Cart/Storage/Session.php
+++ src/Moltin/Cart/Storage/Session.php
@@ -2,68 +2,13 @@
 
 use Moltin\Cart\Item;
 
-class Session implements \Moltin\Cart\StorageInterface
+class Session extends Runtime implements \Moltin\Cart\StorageInterface
 {
-    protected $identifier;
-    protected static $cart = array();
-
     public function __construct()
     {
         session_id() or session_start();
 
         if (isset($_SESSION['cart'])) static::$cart = unserialize($_SESSION['cart']);
-    }
-
-    public function insertUpdate(Item $item)
-    {
-        static::$cart[$this->id][$item->identifier] = $item;
-    }
-
-    public function &data()
-    {
-        return static::$cart[$this->id];
-    }
-
-    public function has($identifier)
-    {
-        foreach (static::$cart[$this->id] as $item) {
-
-            if ($item->identifier == $identifier) return true;
-
-        }
-    }
-
-    public function item($identifier)
-    {
-        foreach (static::$cart[$this->id] as $item) {
-
-            if ($item->identifier == $identifier) return $item;
-
-        }
-    }
-    
-    public function remove($id)
-    {
-        unset(static::$cart[$this->id][$id]);
-    }
-
-    public function destroy()
-    {
-        static::$cart[$this->id] = array();
-    }
-
-    public function setIdentifier($id)
-    {
-        $this->id = $id;
-
-        if ( ! array_key_exists($this->id, static::$cart)) {
-            static::$cart[$this->id] = array();
-        }
-    }
-
-    public function getIdentifier()
-    {
-        return $this->identifier;
     }
 
     public function __destruct()
