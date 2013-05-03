0fe3889aad8bdf9a570753485811694b6dac1750
Chris Harvey <chris@chrisnharvey.com>
2013-05-03 10:50:00 +0100

    Adding copyright information

--- src/Moltin/Cart/Cart.php
+++ src/Moltin/Cart/Cart.php
@@ -1,4 +1,24 @@
-<?php namespace Moltin\Cart;
+<?php
+
+/**
+ * This file is part of Moltin Cart, a PHP package to handle
+ * your shopping basket.
+ *
+ * Copyright (c) 2013 Moltin Ltd.
+ * http://github.com/moltin/cart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package moltin/cart
+ * @author Chris Harvey <chris@molt.in>
+ * @copyright 2013 Moltin Ltd.
+ * @version dev
+ * @link http://github.com/moltin/cart
+ *
+ */
+
+namespace Moltin\Cart;
 
 use InvalidArgumentException;
 
--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * This file is part of Moltin Cart, a PHP package to handle
+ * your shopping basket.
+ *
+ * Copyright (c) 2013 Moltin Ltd.
+ * http://github.com/moltin/cart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package moltin/cart
+ * @author Chris Harvey <chris@molt.in>
+ * @copyright 2013 Moltin Ltd.
+ * @version dev
+ * @link http://github.com/moltin/cart
+ *
+ */
+
 use Moltin\Cart\Cart;
 use Moltin\Cart\Storage\Runtime as RuntimeStore;
 use Moltin\Cart\Identifier\Runtime as RuntimeIdentifier;
